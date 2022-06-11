e94745f4d1f71954fd2a4f058bfc3efa5d45b4a9
JC Flores <juanc.fm.1195@gmail.com>
2022-06-11 00:02:15 -0500

    Add M&T Proyect Eclipse

---
+++ meetbuy/src/main/java/org/generation/meetbuy/MeetbuyApplication.java
@@ -0,0 +1,12 @@
+package org.generation.meetbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetbuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetbuyApplication.class, args);
+	}
+}
---
+++ meetbuy/src/test/java/org/generation/meetbuy/MeetbuyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.meetbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetbuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
