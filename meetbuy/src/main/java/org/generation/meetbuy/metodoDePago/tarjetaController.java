c818571e460e7e865dcadcff8ce9be9df0ab1af7
GuadalupeGerardoCP <guadalupe.gerardo.cp@gmail.com>
2022-06-11 18:49:53 -0500

    creation package metodoDePago

---
+++ meetbuy/src/main/java/org/generation/meetbuy/metodoDePago/tarjetaController.java
@@ -0,0 +1,51 @@
+package org.generation.meetbuy.metodoDePago;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController //estamos diciendo a nuestro controlador que va a manejar la entidad o producto y en este caso, vamos a utilizar metodos GET, POST, PUT y DELETE, le decimos a Spring que esta clase es un controlador
+@RequestMapping(path="/Meet&Buy/tarjeta/")
+@CrossOrigin(origins="http://127.0.0.1:5500/index.html")
+public class tarjetaController {
+	private final tarjetaService tarService;
+	
+	@Autowired
+	
+	public tarjetaController(tarjetaService tarService) {
+		this.tarService = tarService;
+	}
+	
+////////////////////////////Aqui empiezan las solicitudes//////////////////////////////////
+	
+	@GetMapping
+	public List<tarjeta> getTarjeta(){
+		return tarService.getTarjeta(); //quiero que regrese la tarjeta
+	}
+	
+	//Solicitud GET para una sola tarjeta desde su id
+	@GetMapping (path="{tarID}")
+	public tarjeta getTarjeta(@PathVariable("tarID")Long tarId){
+		return tarService.getTarjeta(tarId);
+	}
+	
+	//metodo POST para crear nuevas tarjetas
+	@PostMapping
+	public void addTarjeta(@RequestBody tarjeta tar) {
+		tarService.addTarjeta(tar);
+	}
+	
+	//metodo DELETE para eliminar una tarjeta desde su id
+	@DeleteMapping (path="{tarID}")
+	public void deleteTarjeta(@PathVariable("tarID")Long tarId) {
+		tarService.deleteTarjeta(tarId);
+	}
+}
